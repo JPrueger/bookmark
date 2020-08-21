2003be6a66e14e64adc49eaff6030df997e0f4f3
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-08-21 21:53:30 +0200

    set up footer, nav, index

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+
+<footer>
+
+    <div class="wrapper_whole_content">
+
+        <a href="#">login</a> <br>
+
+        <a href="#">terms of service</a>  <br>
+
+        <a href="#">privacy policy</a>  <br>
+
+        <p>
+            <strong>
+                &copy; 2020 bookmark. 
+            </strong>
+            
+            All rights reserved.
+        </p>
+
+    </div>
+
+</footer>
